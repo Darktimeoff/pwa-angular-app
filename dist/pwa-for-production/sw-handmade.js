18617d1c75317a5a9996a0ba3b568f2b22b7b681
Yevhenii Korolikhin <alexkorolixinofficial@gmail.com>
2021-06-20 15:40:29 +0300

    caching static files

--- dist/pwa-for-production/sw-handmade.js
+++ dist/pwa-for-production/sw-handmade.js
@@ -1,5 +1,20 @@
+var version = 1;
+var cacheStaticName = 's-pwa-for-prod-v' + version;
+var appShellStaticToCache = [
+    './',
+    './index.html',
+    './main.js',
+    './polyfills.js',
+    './styles.css',
+    './runtime.js',
+    './favicon.ico',
+    './assets/icons/icon-512x512.png'
+];
+
 self.addEventListener('install', e => {
-    console.log('[Service Worker]: Installed', e);
+    e.waitUntil(
+        caches.open(cacheStaticName).then(cache => cache.addAll(appShellStaticToCache))
+    )
 })
 
 self.addEventListener('activate', e => {
@@ -8,4 +23,16 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
     console.log('[Service Worker]: Fetch', e)
+    e.respondWith(
+        caches.match(e.request)
+            .then((response) => {
+                if(response) {
+                    console.log('[Service Worker]: Returning' + e.request.url + ' from cache')
+                    return response;
+                } else {
+                    console.log('[Service Worker]: returning' + e.request.url + ' from net')
+                    return fetch(e.request)
+                }
+            })
+    )
 })
